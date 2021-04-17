e4377cef300119dd4300eb9df639d91bc204694a
Tomás García Gobet <tomasgobet@icloud.com>
2021-04-17 10:40:16 +0200

    adds geometric median

--- src/contexts/benchmarks.tsx
+++ src/contexts/benchmarks.tsx
@@ -11,6 +11,7 @@ interface BenchmarksContext {
   deleteMachine: (machineId: string) => void;
   addMachine: (machineId: string, values: number[]) => void;
   getArithmeticMedian: (forMachine: string) => number;
+  getGeometricMedian: (forMachine: string) => number;
 }
 
 const initialState: BenchmarksContext = {
@@ -26,6 +27,7 @@ const initialState: BenchmarksContext = {
   addMachine: (machineId: string, values: number[]) => {},
   deleteMachine: (machineId: string) => {},
   getArithmeticMedian: (forMachine: string) => 0,
+  getGeometricMedian: (forMachine: string) => 0,
 };
 
 const BenchmarksContext = createContext(initialState);
@@ -60,10 +62,36 @@ export const BenchmarksProvider = ({ children }: { children: ReactNode }) => {
     );
   };
 
+  const normalizeBenchmark = (benchmarkId: string, machineId: string) => {
+    const values = { ...benchmarks.find(b => b.id === benchmarkId)?.values };
+    const normalized = { id: benchmarkId, values };
+    const relation = values[machineId] / 100;
+    Object.keys(values)
+      .filter(key => key !== machineId)
+      .forEach(machine => {
+        normalized.values[machine] = normalized.values[machine] / relation;
+      });
+    return normalized;
+  };
+
   const getArithmeticMedian = (forMachine: string) => {
     let value = 0;
     benchmarks.forEach(b => (value += b.values[forMachine]));
     return benchmarks.length > 0 ? value / benchmarks.length : 0;
+  };
+
+  const getGeometricMedian = (forMachine: string) => {
+    const normalizedBenchmarks: Benchmark[] = [];
+    benchmarks.forEach(benchmark => {
+      normalizedBenchmarks.push(normalizeBenchmark(benchmark.id, forMachine));
+    });
+
+    let result = 1;
+    benchmarks.forEach(b => {
+      result = result * b.values[forMachine];
+    });
+
+    return Math.pow(result, 1 / benchmarks.length);
   };
 
   return (
@@ -76,6 +104,7 @@ export const BenchmarksProvider = ({ children }: { children: ReactNode }) => {
         addMachine,
         deleteMachine,
         getArithmeticMedian,
+        getGeometricMedian,
       }}
     >
       {children}
