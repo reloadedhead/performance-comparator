1f99296533323f019adca4f3d9723eb1cc5662a1
Tomás García Gobet <tomasgobet@icloud.com>
2021-04-17 14:34:23 +0200

    fixes layout on small screens

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -8,17 +8,17 @@ const MainPage = () => {
   const { machines, benchmarks } = useBenchmarks();
 
   return (
-    <Grid container spacing={1}>
+    <Grid container spacing={1} justify="space-between">
       <Grid item xs={12}>
         <BenchmarkTable machines={machines} data={benchmarks} />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} md={4}>
         <ArithmeticResults />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} md={4}>
         <GeometricResults />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} md={4}>
         <WeightedResults />
       </Grid>
     </Grid>
